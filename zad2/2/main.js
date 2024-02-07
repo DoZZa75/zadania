483b98db4c60789b0fa324fc46910faebe7acd9e
DoZZa75 <144309313+DoZZa75@users.noreply.github.com>
2024-02-07 12:28:26 +0900

    Add files via upload

---
+++ zad2/2/main.js
@@ -0,0 +1,11 @@
+let numberVar = 10;
+let stringVar = "Hello";
+let booleanVar = true;
+
+numberVar = "New value";
+stringVar = 100; 
+booleanVar = null; 
+
+alert(numberVar);
+alert(stringVar);
+alert(booleanVar);
