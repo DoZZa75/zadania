f73a6ee24f97e3bc908bdb1efc0b2330deeaf3c7
DoZZa75 <144309313+DoZZa75@users.noreply.github.com>
2024-02-09 09:38:11 +0900

    Add files via upload

---
+++ Zad4/index.js
@@ -0,0 +1,19 @@
+const myName = "Стас";
+let programmingLanguage = "JS";
+let beforeprogrammingLanguage = "REP";
+const courseCreatorName = "Никиты Михайловича";
+let reasonText = "Это увлекательная и творческая профессия";
+let numberOfMonth = "1";
+
+programmingLanguage.toUpperCase();
+
+const result = `Всем привет! Меня зовут, ${myName}. Сейчас я изучаю язык программирования \
+ ${programmingLanguage} на курсе по программированию ${programmingLanguage} ${courseCreatorName}. \ 
+ Я хочу стать веб-разработчиком потому что ${reasonText}. До этого я изучал ${beforeprogrammingLanguage} \
+ ${numberOfMonth} месяцев. Я уверен, что пройду данный курс до конца!`;
+console.log(result);
+
+let myInfoText = result;
+
+
+
